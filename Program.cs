1e69278ca4f6466b4f7d4b3ccc50a443b06f26f9
Giovanni <alemangiovanni51@gmail.com>
2024-10-06 17:10:17 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿//Trabajo Realizado por Joseph Saballos 1M2-S
+
+
+
+using System;
+
+namespace SistemaAsistencia
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            double asistenciasTotales = 0;
+
+            // Bienvenida al sistema 
+
+
+            Console.WriteLine("Bienvenido al Sistema de Asistencias de la Universidad Nacional de Ingenieria ");
+            Console.WriteLine();
+
+
+
+
+            Console.WriteLine("Ingrese la Cantidad de Estudiantes");
+            int estudiante = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+
+
+            // Bucle para pedir  las asistencias totales
+            do
+            {
+                Console.WriteLine("Ingrese la Asistencia Total: ");
+                asistenciasTotales = Convert.ToInt32(Console.ReadLine());
+                if (asistenciasTotales < 0)
+                {
+                    Console.WriteLine("Dato erroneo, Ingrese un numero Positivo Por favor");
+                }
+
+            } while (asistenciasTotales < 0);
+
+
+
+
+            //Declaracion de Arreglos 
+
+            string[] cantidadnombres = new string[estudiante];
+            double[] cantidadasistenias = new double[estudiante];
+
+
+
+
+
+            // Arreglo que recopila la cantidad de estudiantes 
+
+
+            for (int i = 0; i < cantidadnombres.Length; i++)
+            {
+                Console.WriteLine();
+
+                Console.WriteLine($"Nombre del Estudiante {i + 1} ");
+                cantidadnombres[i] = Console.ReadLine();
+
+
+
+                // Recopilacion de sesiones que asistio el estudiante 
+                do
+                {
+
+                    Console.WriteLine("Imgrese las Sesiones Asistidas: ");
+                    cantidadasistenias[i] = Convert.ToDouble(Console.ReadLine());
+                    if (cantidadasistenias[i] < 0)
+                    {
+
+                        Console.WriteLine("Dato erroneo, Por favor Ingrese un numero Positivo");
+                    }
+
+
+
+                } while (cantidadasistenias[i] < 0);
+
+                var informacion = new Universidad.Estudiante
+                {
+
+                    nombre = cantidadnombres[i],
+                    numerosesionesTotales = asistenciasTotales,
+                    numerosesionesAsistidas = cantidadasistenias[i],
+
+                };
+
+                informacion.mostrarinfo();
+
+            }
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Los Nombre Ingresados Fueron: ");
+            foreach (var i in cantidadnombres)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            // Mensaje de Despedida
+
+            Console.WriteLine();
+            Console.WriteLine("Gracias por usar nuestro sistema!");
+
+        }
+
+
+    }
+
+}
---
+++ Universidad.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SistemaAsistencia.Universidad;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SistemaAsistencia
+{
+
+
+
+    public class Universidad
+    {
+        public class Estudiante //Clase Principal
+
+        {
+            // Variables para el estudiante 
+
+            public string nombre { get; set; }
+            public double numerosesionesTotales { get; set; }
+            public double numerosesionesAsistidas { get; set; }
+
+
+
+            public class Asistencia //Clase Interna llamada Asistencia 
+            {
+
+                //Metodo para mostrar la asistencia 
+                public static double MostrarPorcentaje(double sesionesAsistidas, double sesionesTotales)
+                {
+
+                    return (double)sesionesAsistidas / sesionesTotales * 100;
+
+                    if (sesionesTotales == 0)
+                    {
+                        return 0;
+                    }
+
+
+
+
+                }
+
+
+            }
+            //Metodo para mostrar la informacion del estudiante 
+            public void mostrarinfo()
+            {
+                Console.WriteLine($"nombre : {nombre} numero de sesiones totales: {numerosesionesTotales}, numero de sesiones asistidas: {numerosesionesAsistidas}");
+
+
+                double asistencia = Asistencia.MostrarPorcentaje(numerosesionesAsistidas, numerosesionesTotales);
+
+                // Validacion de la asistencia 
+
+                if (asistencia >= 75)
+                {
+                    Console.WriteLine("El estudiante cumple con el requisito de asistencia.");
+                }
+                else
+                {
+
+                    Console.WriteLine("Lo siento, el estudiante no cumple con el requisito de asistencia.");
+                }
+
+
+
+
+
+
+            }
+
+        }
+
+
+
+    }
+}
